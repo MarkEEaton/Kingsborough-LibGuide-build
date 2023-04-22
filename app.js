a9f4218a49926aa2f82a396244049a2288aae80f
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-04-22 17:55:31 -0400

    add navcomponent

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import MenusComponent from './menuscomponent.vue'
+import NavComponent from './navcomponent.vue'
 
 const app = createApp({
 	components: {
-		'menus-component': MenusComponent
+		'menus-component': MenusComponent,
+		'nav-component': NavComponent
 	}
 }).mount("#app")
