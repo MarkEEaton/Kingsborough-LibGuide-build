ac034236166b0479e7dd349849c8b47157c24c88
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-04-24 14:24:56 -0400

    first add of onesearch-component

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import HamburgerComponent from './hamburgercomponent.vue'
 import NavComponent from './navcomponent.vue'
+import OnesearchComponent from './onesearchcomponent.vue'
 
 const app = createApp({
 	components: {
 		'hamburger-component': HamburgerComponent,
-		'nav-component': NavComponent
+		'nav-component': NavComponent,
+        'onesearch-component': OnesearchComponent
 	}
 }).mount("#app")
