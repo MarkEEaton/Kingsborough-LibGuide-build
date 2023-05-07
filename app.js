e865dfc9dfd3f32529b9603d8bcb86f93b4d00f9
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-05-06 20:36:26 -0400

    fix indentation in app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ import NavComponent from './navcomponent.vue'
 import OnesearchComponent from './onesearchcomponent.vue'
 
 const app = createApp({
-	components: {
-		'hamburger-component': HamburgerComponent,
-		'nav-component': NavComponent,
+    components: {
+        'hamburger-component': HamburgerComponent,
+        'nav-component': NavComponent,
         'onesearch-component': OnesearchComponent
-	}
+    }
 }).mount("#app")
