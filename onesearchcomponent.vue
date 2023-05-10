924e24f82d1803d2e45f596fbccff28dce373a39
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-05-10 18:33:25 -0400

    more eslinting

--- onesearchcomponent.vue
+++ onesearchcomponent.vue
@@ -1,5 +1,12 @@
 <template>
-<form class="form-inline onesearch-jumbotron" name="searchPrimoForm1" ref="oneSearchForm" role="search" method="get" :action="selectedItem.baseUrl" enctype="application/x-www-form-urlencoded; charset=utf-8">
+<form class="form-inline onesearch-jumbotron"
+      name="searchPrimoForm1"
+      ref="oneSearchForm"
+      role="search"
+      method="get"
+      :action="selectedItem.baseUrl"
+      enctype="application/x-www-form-urlencoded; charset=utf-8"
+>
     <!-- default is "everything"  option suggested from OLS widget builder -->
     <div v-if="selectedItem.input" v-html="selectedItem.input"></div>
     <input name="vid" value="01CUNY_KB:CUNY_KB" type="hidden" />
@@ -20,12 +27,25 @@
     </h1>
     <div class="form-group" id="flex-search-form">
         <div class="dropdown input-group zero-margin" id="blue-border1" ref="blueBorder1">
-            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" ref="oneSearchMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" aria-label="Define Your Search">
-                <span id="defineYourSearch">{{ displayedItem }}&nbsp</span><span class="caret"></span>
+            <button class="btn btn-default dropdown-toggle"
+                    type="button"
+                    id="dropdownMenu1"
+                    ref="oneSearchMenu"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="true"
+                    aria-label="Define Your Search"
+            >
+                <span id="defineYourSearch">{{ displayedItem }}&nbsp;</span>
+                <span class="caret"></span>
             </button>
             <ul class="dropdown-menu fade dropdown-margin" aria-labelledby="dropdownMenu1">
                 <li>
-                    <a v-for="(item, itemName) in materialType" :key="item.id" class="searchmenu" @click="selectDropdown(item, itemName)">
+                    <a v-for="(item, itemName) in materialType"
+                       :key="item.id"
+                       class="searchmenu"
+                       @click="selectDropdown(item, itemName)"
+                    >
                         <div class="highlight-menu-item bigger-fancy-text">
                             <strong>{{ item.fullName }}</strong>
                         </div>
@@ -40,10 +60,21 @@
         </div>
         <label class="sr-only" for="primoQueryTemp">Search terms</label>
         <div class="input-group zero-margin form-width" id="blue-border2">
-            <input class="form-control form-width inherit-height-from-flex" name="queryTemp" id="primoQueryTemp" ref="primoQueryTempString" type="search" v-model="searchString" :placeholder="placeholderString" />
+            <input class="form-control form-width inherit-height-from-flex"
+                   name="queryTemp"
+                   id="primoQueryTemp"
+                   ref="primoQueryTempString"
+                   type="search"
+                   v-model="searchString"
+                   :placeholder="placeholderString"
+             />
         </div>
         <div class="input-group zero-margin" id="blue-border3">
-            <input type="submit" class="btn btn-default form-control onesearchsubmit inherit-height-from-flex" value="Search" @click.stop.prevent="submitSearch"/>
+            <input type="submit"
+                   class="btn btn-default form-control onesearchsubmit inherit-height-from-flex"
+                   value="Search"
+                   @click.stop.prevent="submitSearch"
+            />
         </div>
     </div>
 </form>
@@ -53,29 +84,29 @@
 export default {
   methods: {
     selectDropdown(item, itemName) {
-        this.displayedItem = itemName; // swap in current item at the top of the dropdown
-        this.selectedItem = item; // store the data from the dropdown selection
-        this.placeholderString = "Enter search term here";
+      this.displayedItem = itemName; // swap in current item at the top of the dropdown
+      this.selectedItem = item; // store the data from the dropdown selection
+      this.placeholderString = 'Enter search term here';
     },
     submitSearch() {
-      if ( this.displayedItem == "Define Your Search" ) {
-        this.placeholderString = "Please make a selection";
-        this.searchString = ""; // remove the search string
+      if (this.displayedItem === 'Define Your Search') {
+        this.placeholderString = 'Please make a selection';
+        this.searchString = ''; // remove the search string
         if (!this.$refs.blueBorder1.classList.contains('open')) { // if the dropdown is not open
-            this.$refs.oneSearchMenu.click(); // open the dropdown
+          this.$refs.oneSearchMenu.click(); // open the dropdown
         }
       } else {
-        this.$refs.primoQueryString.value = "any,contains," + this.$refs.primoQueryTempString.value.replace(/[,]/g, " "); // vueified OLS widget code
-        this.$refs.oneSearchForm.submit(); //submit the form
+        this.$refs.primoQueryString.value = `any,contains,${this.$refs.primoQueryTempString.value.replace(/[,]/g, ' ')}`; // vueified OLS widget code
+        this.$refs.oneSearchForm.submit(); // submit the form
       }
-    }
+    },
   },
   data() {
     return {
       selectedItem: {}, // initialize the data from the dropdown selection
-      searchString: "", // initialize an empty search string
-      placeholderString: "Enter search term here", // initialize the placeholder
-      displayedItem: "Define Your Search", // the text displayed a the top of the OneSearch dropdown
+      searchString: '', // initialize an empty search string
+      placeholderString: 'Enter search term here', // initialize the placeholder
+      displayedItem: 'Define Your Search', // the text displayed a the top of the OneSearch dropdown
       materialType: {
         Books: {
           fullName: 'Books',
@@ -83,7 +114,7 @@ export default {
           tab: 'Everything',
           searchScope: 'IZ_CI_AW',
           input: '<input name="facet" value="rtype,include,books" type="hidden" />',
-          id: 1
+          id: 1,
         },
         Articles: {
           fullName: 'Articles (Peer reviewed)',
@@ -91,7 +122,7 @@ export default {
           tab: 'Everything',
           searchScope: 'IZ_CI_AW',
           input: '<input name="mfacet" value="rtype,include,articles,1" type="hidden" /><input name="facet" value="tlevel,include,peer_reviewed" type="hidden" />',
-          id: 2
+          id: 2,
         },
         Reserves: {
           fullName: 'Course reserves',
@@ -99,7 +130,7 @@ export default {
           tab: 'CourseReserves',
           searchScope: 'CourseReserves',
           input: '',
-          id: 3
+          id: 3,
         },
         Newspapers: {
           fullName: 'Newspapers and Magazines',
@@ -107,7 +138,7 @@ export default {
           tab: 'Everything',
           searchScope: 'IZ_CI_AW',
           input: '',
-          id: 4
+          id: 4,
         },
         Journals: {
           fullName: 'Journals',
@@ -115,12 +146,12 @@ export default {
           tab: 'jsearch_slot',
           searchScope: 'IZ_CI_AW',
           input: '',
-          id: 5
+          id: 5,
         },
-      }
-    }
-  }
-}
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
