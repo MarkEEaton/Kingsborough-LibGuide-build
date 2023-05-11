dfe6cf678db9bbb0b31b723edaa8719e5af13cd3
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-05-10 20:16:25 -0400

    eslint build.js app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
-import HamburgerComponent from './hamburger-component.vue'
-import NavComponent from './nav-component.vue'
-import OnesearchComponent from './onesearch-component.vue'
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import HamburgerComponent from './hamburger-component.vue';
+import NavComponent from './nav-component.vue';
+import OnesearchComponent from './onesearch-component.vue';
 
 const app = createApp({
-    components: {
-        'hamburger-component': HamburgerComponent,
-        'nav-component': NavComponent,
-        'onesearch-component': OnesearchComponent
-    }
-}).mount("#app")
+  components: {
+    'hamburger-component': HamburgerComponent,
+    'nav-component': NavComponent,
+    'onesearch-component': OnesearchComponent,
+  },
+}).mount('#app');
--- build.js
+++ build.js
@@ -1,11 +1,11 @@
-const esbuild = require("esbuild")
-const vuePlugin = require("esbuild-plugin-vue3")
+const esbuild = require('esbuild');
+const vuePlugin = require('esbuild-plugin-vue3');
 
 esbuild.build({
-    entryPoints: ["app.js"],
-    logLevel: "info",
-    minify: true,
-    bundle: true,
-    outfile: "out.js",
-    plugins: [vuePlugin()]
-}).catch(() => process.exit(1))
+  entryPoints: ['app.js'],
+  logLevel: 'info',
+  minify: true,
+  bundle: true,
+  outfile: 'out.js',
+  plugins: [vuePlugin()],
+}).catch(() => process.exit(1));
