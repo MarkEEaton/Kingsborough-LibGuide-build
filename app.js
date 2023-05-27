41f09861cd6d6b7d5baf02a60778f04177070581
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-05-27 01:34:10 -0400

    break out links-component

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import FootComponent from './foot-component.vue';
 import HamburgerComponent from './hamburger-component.vue';
+import LinksComponent from './links-component.vue';
 import NavigComponent from './navig-component.vue';
 import OnesearchComponent from './onesearch-component.vue';
 
@@ -8,6 +9,7 @@ const app = createApp({
   components: {
     'foot-component': FootComponent,
     'hamburger-component': HamburgerComponent,
+    'links-component': LinksComponent,
     'navig-component': NavigComponent,
     'onesearch-component': OnesearchComponent,
   },
