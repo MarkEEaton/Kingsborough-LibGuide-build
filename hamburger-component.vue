f47c2c00a276093a9620602c20187a274c7a08c8
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-05-26 21:53:48 -0400

    roll back complexity of hamburger-component

--- hamburger-component.vue
+++ hamburger-component.vue
@@ -1,69 +1,52 @@
 <template>
-<a v-for="item in hamburger"
-   v-bind:key="item.id"
-   class="searchmenu"
-   v-bind:aria-label="item.description"
-   v-bind:href="item.link"
-   v-bind:target="item.target_blank"
->
-  <div class="bigger-fancy-text">
-    <i v-bind:class="item.icon" aria-hidden="true"></i>
-    <strong>{{ item.description }}</strong>
+  <div class="dropdown" id="hamburger-container">
+    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" id="hamburger">
+      <i class="fas fa-bars" style="font-size: 2em;"></i>
+    </button>
+    <ul class="dropdown-menu fade" aria-labelledby="hamburger" id="hamburger-ul">
+      <li>
+        <a class="searchmenu" aria-label="OneSearch" href="https://cuny-kb.primo.exlibrisgroup.com/discovery/search?tab=Everything&vid=01CUNY_KB:CUNY_KB&lang=en">
+          <div class="highlight-menu-item bigger-fancy-text">
+            <i class="fas fa-search fa-fw bigger-icon" aria-hidden="true"></i>
+            <strong>OneSearch</strong>
+          </div>
+        </a>
+        <a class="searchmenu" aria-label="Databases A to Z" href="https://library.kbcc.cuny.edu/az.php">
+          <div class="highlight-menu-item bigger-fancy-text">
+            <i class="fas fa-database fa-fw bigger-icon" aria-hidden="true"></i>
+            <strong>Databases A-Z</strong>
+          </div>
+        </a>
+        <a class="searchmenu" aria-label="Research Guides" href="https://library.kbcc.cuny.edu/guides">
+          <div class="highlight-menu-item bigger-fancy-text">
+            <i class="fas fa-telescope fa-fw bigger-icon" aria-hidden="true"></i>
+            <strong>Research Guides</strong>
+          </div>
+        </a>
+        <a class="searchmenu" aria-label="FAQ" href="https://library.kbcc.cuny.edu/faq">
+          <div class="highlight-menu-item bigger-fancy-text">
+            <i class="fas fa-question-circle fa-fw bigger-icon" aria-hidden="true"></i>
+            <strong>FAQ</strong>
+          </div>
+        </a>
+        <a class="searchmenu" aria-label="Hours" href="https://library.kbcc.cuny.edu/calendar">
+          <div class="highlight-menu-item bigger-fancy-text">
+            <i class="fas fa-clock fa-fw bigger-icon" aria-hidden="true"></i>
+            <strong>Library Hours</strong>
+          </div>
+        </a>
+        <a class="searchmenu" aria-label="Site Map" href="https://library.kbcc.cuny.edu/sitemap">
+          <div class="highlight-menu-item bigger-fancy-text">
+            <i class="fas fa-location-arrow fa-fw bigger-icon" aria-hidden="true"></i>
+            <strong>Site Map</strong>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
-</a>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      hamburger: [
-        {
-          link: 'https://cuny-kb.primo.exlibrisgroup.com/discovery/search?tab=Everything&vid=01CUNY_KB:CUNY_KB&lang=en',
-          icon: 'fas fa-search fa-fw bigger-icon',
-          description: 'OneSearch',
-          target_blank: '',
-          id: 1,
-        },
-        {
-          link: 'https://library.kbcc.cuny.edu/az.php',
-          icon: 'fas fa-database fa-fw bigger-icon',
-          description: 'Databases A to Z',
-          target_blank: '',
-          id: 2,
-        },
-        {
-          link: 'https://library.kbcc.cuny.edu/guides',
-          icon: 'fas fa-telescope fa-fw bigger-icon',
-          description: 'Research Guides',
-          target_blank: '',
-          id: 3,
-        },
-        {
-          link: 'https://library.kbcc.cuny.edu/faq',
-          icon: 'fas fa-question-circle fa-fw bigger-icon',
-          description: 'FAQ',
-          target_blank: '',
-          id: 4,
-        },
-        {
-          link: 'https://library.kbcc.cuny.edu/calendar',
-          icon: 'fas fa-clock fa-fw bigger-icon',
-          description: 'Library Hours',
-          target_blank: '',
-          id: 5,
-        },
-        {
-          link: 'https://library.kbcc.cuny.edu/sitemap',
-          icon: 'fas fa-location-arrow fa-fw bigger-icon',
-          description: 'Site Map',
-          target_blank: '',
-          id: 6,
-        },
-      ],
-    };
-  },
-};
 </script>
 
 <style>
