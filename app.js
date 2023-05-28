927751344ea31c3abe928926b577ab2c1f1288cf
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-05-27 21:32:21 -0400

    split app into 3; add lg-foot-component

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import AlertComponent from './alert-component.vue';
 import FootComponent from './foot-component.vue';
 import HamburgerComponent from './hamburger-component.vue';
+import LgFootComponent from './lg-foot-component.vue';
 import LinksComponent from './links-component.vue';
 import NavigComponent from './navig-component.vue';
 import OnesearchComponent from './onesearch-component.vue';
 
-const app = createApp({
+const app1 = createApp({
   components: {
     'alert-component': AlertComponent,
-    'foot-component': FootComponent,
     'hamburger-component': HamburgerComponent,
-    'links-component': LinksComponent,
     'navig-component': NavigComponent,
     'onesearch-component': OnesearchComponent,
   },
-}).mount('#app');
+}).mount('#app1');
+
+const app2 = createApp({
+  components: {
+    'links-component': LinksComponent,
+  },
+}).mount('#app2');
+
+const app3 = createApp({
+  components: {
+    'foot-component': FootComponent,
+    'lg-foot-component': LgFootComponent,
+  },
+}).mount('#app3');
---
+++ lg-foot-component.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="s-lib-alert" title="">
+    <div id="s-lib-alert-content"></div>
+  </div>
+  <div id="s-lib-popover-title" class="hide">
+    <span class="text-info"><strong>title</strong></span>
+    <button
+      type="button"
+      id="popclose"
+      class="close"
+      onclick="jQuery('.s-lib-popover').popover('hide')"
+    >
+      &times;
+    </button>
+  </div>
+  <div id="s-lib-popover-content" class="hide">
+    <i class="fa fa-refresh fa-spin"></i> Loading...
+    <button class="btn btn-default btn-sm popclose" type="button">Close</button>
+  </div>
+  <!-- end Vue app -->
+  <div id="s-lib-scroll-top" title="Back to Top">
+    <span class="fa-stack fa-lg">
+      <i class="fa fa-square-o fa-stack-2x"></i>
+      <i
+        class="fa fa-angle-double-up fa-stack-1x"
+        style="position: relative; bottom: 2px"
+      ></i>
+    </span>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
